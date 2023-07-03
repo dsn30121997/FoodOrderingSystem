de2aa84a1ceed0412bc3f41001482ab2cbc65102
dhananjay <76154251+dsn30121997@users.noreply.github.com>
2023-07-03 16:27:28 +0530

    Adding Order Items Contriller & views

---
+++ Controllers/OrderItemsController.cs
@@ -0,0 +1,37 @@
+﻿using FoodOrderingSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodOrderingSystem.Controllers
+{
+    public class OrderItemsController : Controller
+    {
+        private FoodOrderingSystemDbContext db = new FoodOrderingSystemDbContext();
+
+        // GET: OrderItems
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Details(int OrderId)
+        {
+            Orders orders = db.Orders.Find(OrderId);
+            //OrderItems orderItem = db.OrderItems.Find(OrderId);
+            List<OrderItems> orderItems = db.OrderItems.Where(oI => oI.OrderId == OrderId).ToList();
+            return View(orderItems);
+
+        }
+
+        //No bugs found.
+
+
+
+
+
+    }
+}
---
+++ Migrations/202307020841018_OrderDatechange.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Entity.Migrations;
+
+public partial class OrderDatechange : DbMigration
+{
+    public override void Up()
+    {
+        AlterColumn("dbo.Orders", "OrderDate", c => c.DateTime(nullable: false));
+    }
+    
+    public override void Down()
+    {
+        AlterColumn("dbo.Orders", "OrderDate", c => c.DateTime(nullable: false));
+    }
+}
